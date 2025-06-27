8fc4b7871cb7262fc3406be7dda32ef276f3e569
Fish <77413091+JulianBarbera@users.noreply.github.com>
2025-06-26 22:28:36 -0700

    Added general functionality

--- open-scope.ino
+++ open-scope.ino
@@ -1,25 +1,37 @@
+// -------------------- Constants --------------------
 #define dirPin 2
 #define stepPin 3
-#define rots 3
 #define scale 200
+#define SUCCESS 0
+#define FAILURE 1
+#define step_per_rot 400
+#define deg_per_step 1 / step_per_rot * 360
 
-bool dir = 0;
+// -------------------- Function Declarations --------------------
+bool raw_move(char axis, float degrees, float time);
 
+// -------------------- Arduino Setup --------------------
 void setup() {
+  Serial.begin(115200);
   // Declare pins as output:
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
-
+  while (!Serial) {} // Wait for serial to begin
+  Serial.println("\nWelcome to Open Scope");
 }
 
-void loop() {
-  digitalWrite(dirPin, dir);
-  dir = !dir;
-  delay(500);
-  for (int i = 0; i < scale * rots; i++) {
+// -------------------- Arduino Loop --------------------
+void loop() {}
+
+// -------------------- Move an Axis --------------------
+bool raw_move(char axis, float degrees, float time) {
+  digitalWrite(dirPin, degrees > 0 ? true : false);
+  float interval = (degrees * deg_per_step) / (2 * time);
+  for (int i = 0; i < abs(degrees) * deg_per_step; i++) {
     digitalWrite(stepPin, HIGH);
-    delay(10);
+    delay(interval);
     digitalWrite(stepPin, LOW);
-    delay(10);
+    delay(interval);
   }
+  return SUCCESS;
 }
