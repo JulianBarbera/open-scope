6105e2f56d556a5e5a2f4cb5abb89640b152b33f
Julia <fish@fishcat.fish>
2025-07-04 13:52:05 -0700

    formatting

--- src/bno.h
+++ src/bno.h
@@ -1,40 +1,45 @@
 #include <Adafruit_BNO055.h>
-bool InitBno(Adafruit_BNO055 &bno)
+bool
+InitBno(Adafruit_BNO055 &bno)
 {
-  bool is_connected = bno.begin();
-  if (!is_connected)
-    Serial.println("Can't connect to Adafruit BNO055!");
+	bool is_connected = bno.begin();
+	if (!is_connected)
+		Serial.println("Can't connect to Adafruit BNO055!");
 
-  /* Use external crystal for better accuracy (taken from BNO055 examples) */
-  bno.setExtCrystalUse(true);
-  return is_connected;
+	/* Use external crystal for better accuracy (taken from BNO055 examples)
+	 */
+	bno.setExtCrystalUse(true);
+	return is_connected;
 }
 
-inline bool IsValidAxis(char axis)
+inline bool
+IsValidAxis(char axis)
 {
-  if (axis >= 'A' && axis <= 'Z')
-    axis += 32;
-  return (axis == 'x' || axis == 'y' || axis == 'z');
+	if (axis >= 'A' && axis <= 'Z')
+		axis += 32;
+	return (axis == 'x' || axis == 'y' || axis == 'z');
 }
-float BnoAxisDeg(Adafruit_BNO055 &bno, char axis)
+float
+BnoAxisDeg(Adafruit_BNO055 &bno, char axis)
 {
-/* Event API found at https://github.com/adafruit/Adafruit_Sensor/blob/master/Adafruit_Sensor.h */
-  sensors_event_t event;
-  bno.getEvent(&event);
-  switch (axis)
-  {
-    case 'x':
-      return event.orientation.x;
-    case 'y':
-      return event.orientation.y;
-    case 'z':
-      return event.orientation.z;
-    default:
-      return event.orientation.x;
-  }
+	/* Event API found at
+	 * https://github.com/adafruit/Adafruit_Sensor/blob/master/Adafruit_Sensor.h
+	 */
+	sensors_event_t event;
+	bno.getEvent(&event);
+	switch (axis) {
+	case 'x':
+		return event.orientation.x;
+	case 'y':
+		return event.orientation.y;
+	case 'z':
+		return event.orientation.z;
+	default:
+		return event.orientation.x;
+	}
 }
 
-void Calibrate(Adafruit_BNO055 &bno)
+void
+Calibrate(Adafruit_BNO055 &bno)
 {
-
 }
--- src/open-scope.cpp
+++ src/open-scope.cpp
@@ -5,7 +5,7 @@
 #include <HardwareSerial.h>
 #include <TinyGPSPlus.h>
 
-#define kDirPin 2
+#define kDirPin	 2
 #define kStepPin 3
 
 HardwareSerial gpsSerial(1);
@@ -32,147 +32,163 @@ double date_dmy = 0;
 u32_t time_cs = 0;
 
 // Setup
-void setup() {
-  Serial.begin(9600);
-  gpsSerial.begin(9600, SERIAL_8N1, 20, 21);
-  pinMode(kStepPin, OUTPUT);
-  pinMode(kDirPin, OUTPUT);
-  while (!Serial) {
-  }
+void
+setup()
+{
+	Serial.begin(9600);
+	gpsSerial.begin(9600, SERIAL_8N1, 20, 21);
+	pinMode(kStepPin, OUTPUT);
+	pinMode(kDirPin, OUTPUT);
+	while (!Serial) {
+	}
 
-  Serial.println("\nWelcome to Open Scope\n");
-  InitBno(bno);
+	Serial.println("\nWelcome to Open Scope\n");
+	InitBno(bno);
 }
 
 // Loop
-void loop() {
-  if (Serial.available()) {
-    String input_buffer = Serial.readStringUntil('\n');
-    input_buffer.trim();
+void
+loop()
+{
+	if (Serial.available()) {
+		String input_buffer = Serial.readStringUntil('\n');
+		input_buffer.trim();
 
-    if (input_buffer.equalsIgnoreCase("help")) {
-      PrintHelpMenu();
-    } else if (!HandleCommand(input_buffer)) {
-      Serial.println(
-          "Unknown command. Use \"help\" to list available commands.\n");
-    }
-  }
+		if (input_buffer.equalsIgnoreCase("help")) {
+			PrintHelpMenu();
+		} else if (!HandleCommand(input_buffer)) {
+			Serial.println(
+			    "Unknown command. Use \"help\" to list available commands.\n");
+		}
+	}
 
-  while (gpsSerial.available() > 0) {
-    gps.encode(gpsSerial.read());
-  }
+	while (gpsSerial.available() > 0) {
+		gps.encode(gpsSerial.read());
+	}
 }
 
 // Command Handling
-bool HandleCommand(const String &input) {
-  if (input.startsWith("raw")) {
-    char axis;
-    float degrees;
-    float time_sec;
+bool
+HandleCommand(const String &input)
+{
+	if (input.startsWith("raw")) {
+		char axis;
+		float degrees;
+		float time_sec;
 
-    int parsed =
-        sscanf(input.c_str(), "raw %c %f %f", &axis, &degrees, &time_sec);
-    if (parsed != 3) {
-      Serial.println("Usage: raw <axis> <degrees> <time_sec>\n");
-      return false;
-    }
-    return RawMove(axis, degrees, time_sec);
-  } else if (input.startsWith("degrees")) {
-    char axis;
-    int parsed = sscanf(input.c_str(), "degrees %c", &axis);
-    if (parsed != 1) {
-      Serial.println("Usage: degrees <axis>\n");
-      return false;
-    }
-    if (!IsValidAxis(axis)) {
-      Serial.println("Error: Invalid axis specified.\n");
-      return true;
-    }
+		int parsed = sscanf(input.c_str(), "raw %c %f %f", &axis,
+				    &degrees, &time_sec);
+		if (parsed != 3) {
+			Serial.println(
+			    "Usage: raw <axis> <degrees> <time_sec>\n");
+			return false;
+		}
+		return RawMove(axis, degrees, time_sec);
+	} else if (input.startsWith("degrees")) {
+		char axis;
+		int parsed = sscanf(input.c_str(), "degrees %c", &axis);
+		if (parsed != 1) {
+			Serial.println("Usage: degrees <axis>\n");
+			return false;
+		}
+		if (!IsValidAxis(axis)) {
+			Serial.println("Error: Invalid axis specified.\n");
+			return true;
+		}
 
-    Serial.println(BnoAxisDeg(bno, axis));
-    Serial.println();
-    return true;
-  } else if (input.startsWith("gps-print")) {
-    DisplayGPS();
-    return false;
-  }
+		Serial.println(BnoAxisDeg(bno, axis));
+		Serial.println();
+		return true;
+	} else if (input.startsWith("gps-print")) {
+		DisplayGPS();
+		return false;
+	}
 
-  return false;
+	return false;
 }
 
 // Help Menu
-void PrintHelpMenu() {
-  Serial.println();
-  Serial.println("Welcome to Open Scope!");
-  Serial.println("Available commands:");
-  Serial.println("    help              - Show this menu");
-  Serial.println("    raw <a> <d> <t>   - Move axis <a> by <d>° in <t> sec");
-  Serial.println("                       e.g. raw X 90 2.0");
-  Serial.println("    gps-print         - Print out all GPS data");
-  Serial.println();
+void
+PrintHelpMenu()
+{
+	Serial.println();
+	Serial.println("Welcome to Open Scope!");
+	Serial.println("Available commands:");
+	Serial.println("    help              - Show this menu");
+	Serial.println(
+	    "    raw <a> <d> <t>   - Move axis <a> by <d>° in <t> sec");
+	Serial.println("                       e.g. raw X 90 2.0");
+	Serial.println("    gps-print         - Print out all GPS data");
+	Serial.println();
 }
 
 // Move Command
-bool RawMove(char axis, float degrees, float time_sec) {
-  (void)axis; // axis currently unused
+bool
+RawMove(char axis, float degrees, float time_sec)
+{
+	(void)axis; // axis currently unused
 
-  digitalWrite(kDirPin, degrees > 0 ? LOW : HIGH);
+	digitalWrite(kDirPin, degrees > 0 ? LOW : HIGH);
 
-  int step_count = round(abs(degrees) * kStepPerDeg);
-  if (step_count == 0 || time_sec <= 0) {
-    Serial.println("Invalid parameters for movement.");
-    return false;
-  }
+	int step_count = round(abs(degrees) * kStepPerDeg);
+	if (step_count == 0 || time_sec <= 0) {
+		Serial.println("Invalid parameters for movement.");
+		return false;
+	}
 
-  float interval_ms = (time_sec * 1000) / (2 * step_count);
+	float interval_ms = (time_sec * 1000) / (2 * step_count);
 
-  Serial.println("Performing raw move.");
-  Serial.print("Steps: ");
-  Serial.println(step_count);
-  Serial.print("Interval: ");
-  Serial.print(interval_ms);
-  Serial.println(" ms\n");
+	Serial.println("Performing raw move.");
+	Serial.print("Steps: ");
+	Serial.println(step_count);
+	Serial.print("Interval: ");
+	Serial.print(interval_ms);
+	Serial.println(" ms\n");
 
-  for (int i = 0; i < step_count; ++i) {
-    digitalWrite(kStepPin, HIGH);
-    delay(interval_ms);
-    digitalWrite(kStepPin, LOW);
-    delay(interval_ms);
-  }
+	for (int i = 0; i < step_count; ++i) {
+		digitalWrite(kStepPin, HIGH);
+		delay(interval_ms);
+		digitalWrite(kStepPin, LOW);
+		delay(interval_ms);
+	}
 
-  Serial.println("Raw move complete.\n");
-  return true;
+	Serial.println("Raw move complete.\n");
+	return true;
 }
 
-bool UpdateGPS() {
-  if (gps.location.isValid()) {
-    sats = gps.satellites.value();
-    lat_d = gps.location.lat();
-    lng_d = gps.location.lng();
-    alt_m = gps.altitude.meters();
-    date_dmy = gps.date.value();
-    time_cs = gps.time.value();
-    return true;
-  }
-  return false;
+bool
+UpdateGPS()
+{
+	if (gps.location.isValid()) {
+		sats = gps.satellites.value();
+		lat_d = gps.location.lat();
+		lng_d = gps.location.lng();
+		alt_m = gps.altitude.meters();
+		date_dmy = gps.date.value();
+		time_cs = gps.time.value();
+		return true;
+	}
+	return false;
 }
 
-void DisplayGPS() {
-  if (UpdateGPS()) {
-    Serial.print("num sats: ");
-    Serial.print(sats);
-    Serial.print(" lat: ");
-    Serial.print(lat_d);
-    Serial.print(" lng: ");
-    Serial.print(lng_d);
-    Serial.print(" alt: ");
-    Serial.print(alt_m);
-    Serial.print(" date: ");
-    Serial.print(date_dmy);
-    Serial.print(" time: ");
-    Serial.print(time_cs);
-    Serial.println();
-  } else {
-    Serial.println("Satellite error.");
-  }
+void
+DisplayGPS()
+{
+	if (UpdateGPS()) {
+		Serial.print("num sats: ");
+		Serial.print(sats);
+		Serial.print(" lat: ");
+		Serial.print(lat_d);
+		Serial.print(" lng: ");
+		Serial.print(lng_d);
+		Serial.print(" alt: ");
+		Serial.print(alt_m);
+		Serial.print(" date: ");
+		Serial.print(date_dmy);
+		Serial.print(" time: ");
+		Serial.print(time_cs);
+		Serial.println();
+	} else {
+		Serial.println("Satellite error.");
+	}
 }
